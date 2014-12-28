be89f3636a40f8742683ec862b2fcc9e1e819645
rliuzzi <rliuzzi@gmail.com>
2014-12-28 20:53:17 +0100

    add missing files

---
+++ app/src/main/java/com/zzivi/sodexo/base/datasource/sharedpreferences/SessionDataSource.java
@@ -0,0 +1,23 @@
+package com.zzivi.sodexo.base.datasource.sharedpreferences;
+
+import com.zzivi.sodexo.login.datasource.api.model.CookiesResultModel;
+import com.zzivi.sodexo.login.domain.model.LoginCredentials;
+
+/**
+ * Created by daniel on 28/09/14.
+ */
+public interface SessionDataSource {
+    public void storeLoginCookie(CookiesResultModel cookiesResultModel);
+
+    public void deleteLoginCookie ();
+
+    public CookiesResultModel obtainLoginCookie();
+
+    public boolean isLoggedIn();
+
+    public void setLoggedIn(boolean isLogged);
+
+    public void storeCredentials(LoginCredentials LoginCredentials);
+
+    public LoginCredentials obtainCredentials();
+}
---
+++ app/src/main/java/com/zzivi/sodexo/base/datasource/sharedpreferences/imp/SessionDataSourceSharedPreferences.java
@@ -0,0 +1,86 @@
+package com.zzivi.sodexo.base.datasource.sharedpreferences.imp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.zzivi.sodexo.base.daggerutils.ForApplication;
+import com.zzivi.sodexo.base.datasource.sharedpreferences.SessionDataSource;
+import com.zzivi.sodexo.login.datasource.api.model.CookiesResultModel;
+import com.zzivi.sodexo.login.domain.model.LoginCredentials;
+
+import javax.inject.Inject;
+
+/**
+ * Created by daniel on 28/09/14.
+ */
+public class SessionDataSourceSharedPreferences implements SessionDataSource {
+    private static final String AUTHORIZE_FILE = "AUTHORIZE";
+
+    public final Context context;
+
+    @Inject
+    public SessionDataSourceSharedPreferences(@ForApplication Context context){
+        this.context = context;
+    }
+
+    @Override
+    public void storeLoginCookie(CookiesResultModel cookiesResultModel) {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        String cookie = cookiesResultModel.getCookie();
+        if (cookie != null) {
+             editor.putString("cookie", cookie);
+        }
+        editor.commit();
+    }
+
+    @Override
+    public void deleteLoginCookie() {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.remove("cookie");
+        editor.commit();
+    }
+
+
+    @Override
+    public CookiesResultModel obtainLoginCookie() {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+
+        CookiesResultModel cookiesResultModel = new CookiesResultModel();
+        cookiesResultModel.setCookie(settings.getString("cookie", "not found"));
+        return cookiesResultModel;
+    }
+
+    @Override
+    public void storeCredentials(LoginCredentials loginCredentials) {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("username", loginCredentials.getUsername());
+        editor.putString("password", loginCredentials.getPassword());
+        editor.commit();
+    }
+
+    @Override
+    public LoginCredentials obtainCredentials() {
+        LoginCredentials loginCredentials = new LoginCredentials();
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        loginCredentials.setUsername(settings.getString("username", ""));
+        loginCredentials.setPassword(settings.getString("password", ""));
+        return loginCredentials;
+    }
+
+    @Override
+    public boolean isLoggedIn() {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        return settings.getBoolean("isLoggedIn", false);
+    }
+
+    @Override
+    public void setLoggedIn(boolean isLoggedIn) {
+        SharedPreferences settings = context.getSharedPreferences(AUTHORIZE_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("isLoggedIn", isLoggedIn);
+        editor.commit();
+    }
+}
